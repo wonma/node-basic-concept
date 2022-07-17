825ddeb6b9bffd0b0df85cf33f843373497c04b7
wonma <wonmadesign@gmail.com>
2022-07-17 14:24:16 -0500

    Learn post request with req.body

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.get('/', (req, res) => {
+  console.log(req.body);
   res.send('<h1>holaaaaaaa</h1>');
+});
+
+app.post('/profile', (req, res) => {
+  console.log(req.body);
+  res.send('success');
 });
 
 app.listen(3000);
